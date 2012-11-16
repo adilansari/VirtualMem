6f31367a6f6abefdb1f020ec2b81a2521b3d5ad4
adil <mansari@buffalo.edu>
2012-11-16 14:59:56 -0500

    FIFO algorithm implemented as a separate program.

--- fifo.c
+++ fifo.c
@@ -1,8 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "virtualmem.c"
+void fifo_eval(int orig_str[], int alloc_frames, int str_size) {
 
-void fifo_eval() {
+	printf("New file");
 
+	int page_table[alloc_frames];
+	int index,flag,i;
+	int j,fault_count=0;
+	printf("incoming values %d %d \n", alloc_frames, str_size);
+	for(i=0;i<str_size;i++) {
+		printf("%d,", orig_str[i]);
+	}
+	for (i=0;i<str_size;i++) {
+		//int x,y,z=0;
+		flag=0;
+		/*printf("\n alpha 1");
+		printf(" page table now: ");
+						while(x<alloc_frames){
+							printf("%d ",page_table[x++]);
+						}*/
+		for(index=0;index<alloc_frames;index++) {
+			if(orig_str[i] == page_table[index]) {
+				flag=1;
+				/*j=index+1;
+				printf("\n alpha 3");
+				printf(" page table now: ");
+				while(y<alloc_frames) {
+					printf("%d ", page_table[y]);
+					y++;
+				}
+				break;*/
+			}
+		}
+		if(flag==0) {
+						page_table[j%alloc_frames]= orig_str[i];
+						fault_count++;
+						j++;
+						/*printf("\n alpha 2");
+						printf(" page table now: ");
+								while(z<alloc_frames){
+										printf("%d ",page_table[z]);
+										z++;
+								}*/
+							}
+	}
+	printf("Total faults: %d \n", fault_count);
+}
+
+void main() {
+	int orig_str[]= {1,2,3,4,1,2,5,1,2,3,4,5};
+	fifo_eval(orig_str,4,12);
 }
--- virtualmem.c
+++ virtualmem.c
@@ -17,9 +17,13 @@ Class 9: Print results
 #define BUF_SIZE 1024
 
 void usage();
+void algo();
 
-extern int str[BUF_SIZE];
+int str[BUF_SIZE];
 int j=0;
+int frames = 5;
+char *filename;
+int algo_flag=1;
 
 int main(int argc, char *argv[]) {
 
@@ -30,38 +34,62 @@ int main(int argc, char *argv[]) {
 					usage();
 		}
 		if(strcmp(argv[i],"-f")==0) {
-					//some function
+			frames= atoi(argv[i+1]);
+			printf("\n gamma 0");
 				}
 		if(strcmp(argv[i],"-r")==0) {
-					//some function
+					if(strcmp(argv[i+1],"LFU")==0) {
+						algo_flag=2;
+					}
+					else if(strcmp(argv[i+1],"LRU-STACK")==0) {
+						algo_flag=3;
+					}
+					else if(strcmp(argv[i+1],"LRU-CLOCK")==0) {
+						algo_flag=4;
+					}
+					else if(strcmp(argv[i+1],"LRU-REF8")==0) {
+						algo_flag=5;
+					}
+					else {
+						//FIFO
+						printf("\n gamma 1");
+					}
 				}
 		if(strcmp(argv[i],"-i")==0) {
-					//some function
+			filename= argv[i+1];
 				}
 
 	}
 
+	/*code to verify parameters have been updated
+	printf("Frames= %d\n", frames);
+	printf("Filename:%s \n", filename);*/
+
 	// file reader goes here
-	printf("Filename:%s \n", argv[1]);
 	FILE *fp;
-	fp= fopen(argv[1], "r");
+	fp= fopen(filename, "r");
 
 	if (fp == NULL) {
 		printf("Cannot open file %s \n", argv[1]);
 	}
 	else {
 		char s[BUF_SIZE];
-		int c,k=0;
+		int c,flag;
+		int k=0;
 		size_t nread;
 		if (fp) {
-			while ((nread = fread(s, 1, sizeof s, fp)) > 0)
-			        fwrite(s, 1, nread, stdout);
+			/*while ((nread = fread(s, 1, sizeof s, fp)) > 0)
+			        fwrite(s, 1, nread, stdout);*/
+			while ((s[k] = fgetc(fp)) != EOF) {
+					//printf("%c\n", c);
+						k++;
+					}
 		}
 		fclose(fp);
 		//copy buffer to int array
 		for(c=0;s[c]!='\0';c++) {
 			k=0;
-			int flag=0;
+			flag=0;
 			while(!isspace(s[c])) {
 				k= k*10 + s[c]-'0';
 				c++;
@@ -72,12 +100,38 @@ int main(int argc, char *argv[]) {
 		}
 		j--;
 	}
-	printf("Captured Int array\n");
-	int k;
-	for(k=0;k<j;k++) {
-		printf("%d \n",str[k]);
+	printf("\n Captured Int array \n");
+	int p;
+	for(p=0;p<j;p++) {
+		printf("%d ",str[p]);
 	}
+	printf("\n gamma 3");
+	algo();
 	return 0;
+}
+
+void algo() {
+	printf("\n gamma 4 \n");
+	if (algo_flag == 2) {
+		//lfu
+		printf("\n gamma 5 \n");
+	}else if(algo_flag == 3) {
+		//lru-stack
+		printf("\n gamma 6 \n");
+	}else if(algo_flag == 4) {
+		//lru-clock
+	} else if(algo_flag == 5) {
+		//lru-ref8
+	} else {
+		printf("\n gamma 2 \n");
+		printf("%d %d \n", frames,j);
+		printf("\n Captured Int array \n");
+			int k;
+			for(k=0;k<j;k++) {
+				printf("%d ",str[k]);
+			}
+		//fifo_eval(str,frames,j);
+	}
 }
 
 void usage() {
